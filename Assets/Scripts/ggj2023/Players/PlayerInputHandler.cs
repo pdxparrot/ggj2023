c600c3eb2853a79f7e7a123ea3dc4c1b3901dda5
Shane Lillie <ignignokterr@gmail.com>
2023-01-30 21:53:12 -0800

    Player jumps now

--- Assets/Scripts/ggj2023/Players/PlayerInputHandler.cs
+++ Assets/Scripts/ggj2023/Players/PlayerInputHandler.cs
@@ -1,7 +1,9 @@
+using pdxpartyparrot.Game.Characters.BehaviorComponents;
 using pdxpartyparrot.Game.Players.Input;
 using pdxpartyparrot.ggj2023.Data.Players;
 
 using UnityEngine.Assertions;
+using UnityEngine.InputSystem;
 
 namespace pdxpartyparrot.ggj2023.Players
 {
@@ -17,6 +19,43 @@ namespace pdxpartyparrot.ggj2023.Players
 
             Assert.IsTrue(PlayerInputData is PlayerInputData);
             Assert.IsTrue(Player is Player);
+        }
+
+        #endregion
+
+        #region Actions
+
+        public void OnJumpAction(InputAction.CallbackContext context)
+        {
+            if(!IsInputAllowed(context)) {
+                return;
+            }
+
+            if(context.performed) {
+                GamePlayer.PlayerBehavior.ActionPerformed(JumpBehaviorComponent.JumpAction.Default);
+            }
+        }
+
+        public void OnAttackAction(InputAction.CallbackContext context)
+        {
+            if(!IsInputAllowed(context)) {
+                return;
+            }
+
+            if(context.performed) {
+                //GTODO: amePlayer.PlayerBehavior.ActionPerformed(BeaverBehaviorComponent.AttackAction.Default);
+            }
+        }
+
+        public void OnStrongAttackAction(InputAction.CallbackContext context)
+        {
+            if(!IsInputAllowed(context)) {
+                return;
+            }
+
+            if(context.performed) {
+                //TODO: GamePlayer.PlayerBehavior.ActionPerformed(BeaverBehaviorComponent.StrongAttackAction.Default);
+            }
         }
 
         #endregion
