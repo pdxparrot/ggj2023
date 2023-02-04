3474f8be081f9e34a9acf69ae7d23c7d8582e1e6
Shane Lillie <ignignokterr@gmail.com>
2023-02-04 10:06:52 -0800

    Add the beaver model script

---
+++ Assets/Scripts/ggj2023/Players/BeaverModel.cs
@@ -0,0 +1,11 @@
+using pdxpartyparrot.Core.Animation;
+
+using UnityEngine;
+
+namespace pdxpartyparrot.ggj2023.Players
+{
+    [RequireComponent(typeof(SpineAnimationHelper))]
+    public sealed class BeaverModel : MonoBehaviour
+    {
+    }
+}
